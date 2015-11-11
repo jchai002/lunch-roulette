27d383c1aed4b3f7c0f6ae806dbcf55e62869572
Jerry Chai <jchai002@gmail.com>
2015-11-11 12:02:39 -0800

    add map to display current location

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
-	var lng = $("#something_long");
-	var lat = $("#something_lat");
+	var lng = $("#search_long");
+	var lat = $("#search_lat");
 	
 function getLocation() {
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setPosition);
-    }
-}
-function setPosition(position) {
-    lat.val(position.coords.latitude)
-     lng.val(position.coords.longitude)
-     console.log(lng.val(),lat.val())
-    
-}
+	    }
+	}
 
+	function setPosition(position) {
+	    lat.val(position.coords.latitude)
+	    lng.val(position.coords.longitude)
+	    var latitude = parseFloat(lat.val())
+	    var longitude = parseFloat(lng.val())
+
+		initMap(longitude,latitude)
+	    
+	}
+
+	function initMap(longitude,latitude) {
+		var mapCenter = {lat: latitude, lng: longitude}
+	    var mapOptions = {
+	      zoom: 16,
+	      center: mapCenter
+	    }
+	    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	    var marker = new google.maps.Marker({
+	    position: mapCenter,
+	    map: map
+	  });
+
+	}
+	
 getLocation()
 
  
